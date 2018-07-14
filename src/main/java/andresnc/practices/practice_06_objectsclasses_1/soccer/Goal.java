86f6de0d5af538c8ac1f6a631098c974c5bdb09b
Andres <andres96-nc@hotmail.com>
2018-07-14 12:31:32 -0500

    Agregando practica 6.1

---
+++ src/main/java/andresnc/practices/practice_06_objectsclasses_1/soccer/Goal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andresnc.practices.practice_06_objectsclasses_1.soccer;
+
+/**
+ *
+ * @author andres
+ */
+public class Goal {
+    public Team theTeam;
+    public Player thePLayer;
+    public double theTime;
+}
---
+++ src/main/java/andresnc/practices/practice_06_objectsclasses_1/soccer/Team.java
@@ -0,0 +1,15 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andresnc.practices.practice_06_objectsclasses_1.soccer;
+
+/**
+ *
+ * @author andres
+ */
+public class Team {
+    public String teamName;
+    public Player[] playerArray;
+}
