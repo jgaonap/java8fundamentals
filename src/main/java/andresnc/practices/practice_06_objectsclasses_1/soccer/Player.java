ea7c1c7e75a0346d515ff0228d704c85dc9f95d0
Andres <andres96-nc@hotmail.com>
2018-07-14 12:32:37 -0500

    Agregando clase Player

--- src/main/java/andresnc/practices/practice_06_objectsclasses_1/soccer/Player.java
+++ src/main/java/andresnc/practices/practice_06_objectsclasses_1/soccer/Player.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package soccer;
+package andresnc.practices.practice_06_objectsclasses_1.soccer;
 
 /**
  *
