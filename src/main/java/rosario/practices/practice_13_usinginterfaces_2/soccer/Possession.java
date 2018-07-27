12db1c60beae80c7f598100213cc2350169cb923
Rosario Cruz <rosario7.rcruze@gmail.com>
2018-07-27 17:38:17 -0500

    se agregan practicas de la 10 a la 14

---
+++ src/main/java/rosario/practices/practice_13_usinginterfaces_2/soccer/Possession.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rosario.practices.practice_13_usinginterfaces_2.soccer;
+
+/**
+ *
+ * @author rosario
+ */
+public class Possession extends GameEvent {
+    public String toString(){
+        return "Possession";
+    }
+}
---
+++ src/main/java/rosario/practices/practice_14_exceptions_1/utility/PlayerDatabaseException.java
@@ -0,0 +1,11 @@
+package rosario.practices.practice_14_exceptions_1.utility;
+
+/**
+ *
+ * @author rosario
+ */
+public class PlayerDatabaseException extends Exception{
+    public PlayerDatabaseException(String message){
+        super(message);
+    }
+}
